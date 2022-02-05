98c7e20d7bca246e97da870d619e4f7f41ce247c
theo543 <81699222+theo543@users.noreply.github.com>
2022-02-05 20:51:00 +0200

    Refactor html script

--- tablecheckboxes.js
+++ tablecheckboxes.js
@@ -1,88 +1,40 @@
-function restoreall() {
-    elements = document.querySelectorAll(".hide, .highlighted");
-    for (let elem of elements) {
-        elem.classList.remove("hide");
-        elem.classList.remove("highlighted");
-    }
-    elements = document.getElementsByTagName("input");
-    for (let elem of elements) {
-        elem.checked = false;
-    }
-    altered = 0;
-    update_resetter();
-}
+'use strict';
+
 var resetelem = document.getElementById("resetter");
-resetelem.addEventListener("click", restoreall);
-function update_resetter() {
-    if (altered) {
-        resetelem.classList.remove("hide");
-    } else {
-        resetelem.classList.add("hide");
-    }
-}
-function highlightTag(event) {
-    element = event.target;
-    tags = element.parentNode.previousElementSibling.textContent.toLowerCase().replace(/\s/g, "").replace(/\n/g, "").split(",");
-    alltags = document.getElementsByTagName("*");
-    if (element.checked) {
-        for (let tag of alltags) {
-            if (tags.includes(tag.tagName.toLowerCase())) {
-                tag.classList.add("highlighted");
-            }
-        }
-        altered++;
-    } else {
-        for (let tag of alltags) {
-            if (tags.includes(tag.tagName.toLowerCase())) {
-                tag.classList.remove("highlighted");
-            }
-        }
-        altered--;
-    }
-    update_resetter();
-}
-function hideTag(event) {
-    element = event.target;
-    tags = element.parentNode.previousElementSibling.textContent.toLowerCase().replace(/\s/g, "").replace(/\n/g, "").split(",");
-    alltags = document.getElementsByTagName("*");
-    if (element.checked) {
-        for (let tag of alltags) {
-            if (tags.includes(tag.tagName.toLowerCase())) {
-                tag.classList.add("hide");
-            }
-        }
-        altered++;
-    } else {
-        for (let tag of alltags) {
-            if (tags.includes(tag.tagName.toLowerCase())) {
-                tag.classList.remove("hide");
-            }
-        }
-        altered--;
-    }
-    update_resetter();
-}
+resetelem.addEventListener("click", (function(){
+    document.querySelectorAll("form").forEach(e => e.none.click());
+}));
+
 var altered = 0;
-cells = document.querySelectorAll("table.togglegoeshere tr td:nth-child(2)");
-for (let cell of cells) {
-    cell.appendChild(document.createElement("br"));
-    check_e = document.createElement("input");
-    check_e.setAttribute("type", "checkbox");
-    cell.appendChild(check_e);
-    text_e = document.createElement("span");
-    text_e.textContent = "Highlight Tags";
-    text_e.className = "removeborder";
-    cell.appendChild(text_e);
-    check_e.addEventListener("change", highlightTag);
-    if (!(cell.classList.contains("nochkbox"))) {
-        cell.appendChild(document.createElement("br"));
-        check_e = document.createElement("input");
-        check_e.setAttribute("type", "checkbox");
-        cell.appendChild(check_e);
-        text_e = document.createElement("span");
-        text_e.textContent = "Hide Tags";
-        text_e.className = "removeborder";
-        cell.appendChild(text_e);
-        check_e.addEventListener("change", hideTag);
+var cells = document.querySelectorAll("#toggletable tr td:nth-child(2)");
+var tracker = 0;
+function updateClasses(event){
+    form = event.currentTarget
+    for(var tag of document.querySelectorAll(form.selector)){
+        tag.classList.toggle("hide", form.hide.checked);
+        tag.classList.toggle("highlighted", form.highlight.checked);
     }
+    if(form.prevWasNone != form.none.checked){
+        altered += form.none.checked ? -1 : 1;
+        form.prevWasNone = form.none.checked;
+    }
+    resetelem.classList.toggle("hide", altered == 0);
+}
+for (var cell of cells) {
+    var div = cell.appendChild(document.createElement("div"));
+    div.innerHTML =
+    `<form>
+    <label><input type=\"radio\" name=r>None</label>
+    <label><input type=\"radio\" name=r>Hide</label>
+    <label><input type=\"radio\" name=r>Highlisht</label>
+    </form>`;
+    var form = div.querySelectorAll("form")[0];
+    form.prevWasNone = true;
+    form.none = div.querySelectorAll("input")[0]
+    form.hide = div.querySelectorAll("input")[1];
+    form.highlight = div.querySelectorAll("input")[2];
+    form.selector = cell.previousElementSibling.innerText.toLowerCase().trim().replaceAll(/\n| /g, ",");
+    form.addEventListener("change", updateClasses);
+    form.none.click()
+    tracker++;
 }
