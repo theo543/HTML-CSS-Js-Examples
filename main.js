c81d66b4839b1613049240671d3c907fdb4259ea
theo543 <81699222+theo543@users.noreply.github.com>
2022-02-22 23:30:54 +0200

    Convert to SCSS

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const path = require("path");
+const sass = require("node-sass-middleware")
 
 const app = express()
 const port = process.env.PORT || 80
 
+app.use("/", sass({
+    src: path.join(__dirname, 'public'),
+    dest: path.join(__dirname, 'public'),
+    outputStyle: 'compressed'
+}));
 app.use("/", express.static(path.join(__dirname, "public")));
 app.listen(port);
