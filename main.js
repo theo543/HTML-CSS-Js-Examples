6643892ed861de25670a4cb2f74a237603caef53
theo543 <81699222+theo543@users.noreply.github.com>
2022-02-23 21:12:57 +0200

    Set up postcss with autoprefixer and cssnano

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const express = require('express')
 const path = require("path");
 const sass = require("node-sass-middleware")
+const postcss = require("postcss-middleware")
 
 const app = express()
 const port = process.env.PORT || 80
 
-app.use("/", sass({
-    src: path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public'),
-    outputStyle: 'compressed'
+app.use(sass({
+        src: path.join(__dirname, 'public'),
+        outputStyle: 'nested'
+}));
+app.use(/^.*\.css$/, postcss({
+        plugins: [require("autoprefixer"), require("cssnano")],
+        src: function(req) {
+            return path.join(__dirname, "public", req.originalUrl);
+        }
 }));
 app.use("/", express.static(path.join(__dirname, "public")));
 app.listen(port);
