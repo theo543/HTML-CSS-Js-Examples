d1efa40d3ee0de5635f0f08cd78ed5fcc64d9ecc
theo543 <81699222+theo543@users.noreply.github.com>
2022-02-25 10:50:31 +0200

    Convert files to pug, set up express rendering

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require("path");
 const sass = require("node-sass-middleware")
 const postcss = require("postcss-middleware")
+const views = ["/index", "/js", "/css", "/html"]
 
 const app = express()
 const port = process.env.PORT || 80
@@ -10,7 +11,7 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.use(sass({
         src: path.join(__dirname, 'public'),
-        outputStyle: 'nested'
+        outputStyle: 'compressed'
 }));
 app.use(/^.*\.css$/, postcss({
         plugins: [require("autoprefixer"), require("cssnano")],
@@ -18,6 +19,12 @@ app.use(/^.*\.css$/, postcss({
             return path.join(__dirname, "public", req.originalUrl);
         }
 }));
+app.all("/", ((req, res, next) => res.redirect("/index")));
+app.all("*.pug", ((req, res, next) => res.redirect(req.originalUrl.slice(0, -4))));
+app.all(views, ((req, res, next) => {
+        const p = path.join(__dirname, "public", req.originalUrl) + ".pug";
+        res.render(p, {path: p});
+}))
 app.use("/", express.static(path.join(__dirname, "public")));
 app.listen(port);
 console.log(`Listening on ${port}!`);
--- public/scripts/CSSToggle.js
+++ public/scripts/CSSToggle.js
@@ -1,5 +1,9 @@
-const link = document.getElementById("css-tag");
+const link = document.querySelector("head link[rel='stylesheet']");
 function toggleAll(){
+    if(!link){
+        console.log("Failed to find CSS tag!");
+        return;
+    }
     if(document.contains(link)){
         link.parentNode.removeChild(link);
     }else{
