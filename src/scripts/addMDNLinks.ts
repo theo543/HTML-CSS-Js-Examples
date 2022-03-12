946956824dcf69bbe9f527930a63b2784d61fed1
theo543 <81699222+theo543@users.noreply.github.com>
2022-03-12 10:58:32 +0200

    Convert addMDNLinks to TypeScript

--- src/scripts/addMDNLinks.ts
+++ src/scripts/addMDNLinks.ts
@@ -1,10 +1,12 @@
 'use strict';
 (function (){
-    for(const table of document.querySelectorAll(".mdn-link-target-container")) {
-        for(const cell of table.querySelectorAll(table.getAttribute("data-mdn-link-selector"))) {
+    document.querySelectorAll(".mdn-link-target-container[data-mdn-link-selector]").forEach((table) => {
+        table.querySelectorAll(table.getAttribute("data-mdn-link-selector")!).forEach((cell) => {
             cell.childNodes.forEach((node) => {
                 if(node.nodeType === Node.TEXT_NODE) {
                     const newElement = document.createElement("span");
+                    if(!node.textContent)
+                        return;
                     newElement.innerHTML = node.textContent.replace(/[a-z][a-z0-9]*/, (tagName) => {
                         const docURL = `https://developer.mozilla.org/en-US/docs/Web/HTML/Element/${tagName}`;
                         return `<a href="${docURL}">${tagName}</a>`;
@@ -12,6 +14,6 @@
                     node.replaceWith(newElement);
                 }
             });
-        }
-    }
+        });
+    });
 })();
